e5f2d28c660df37e66fae8eae556f5bacb9b0d0d
Eric Kudler <joeento@gmail.com>
2023-01-02 21:44:09 -0500

    Adds cosine similarity but does nothing with it yet

--- match.py
+++ match.py
@@ -3,6 +3,9 @@ import cv2
 import mediapipe as mp
 import time
 
+from numpy import dot
+from numpy.linalg import norm
+
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 mpDraw = mp.solutions.drawing_utils
@@ -10,22 +13,39 @@ mpDraw = mp.solutions.drawing_utils
 SOURCE_DIR ='source/'
 DEST_DIR = 'dest/'
 
-def match(filename):
-    img = cv2.imread(SOURCE_DIR + filename)
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+def cosine_similarity(A, B):
+    cos_sim = dot(a, b)/(norm(a)*norm(b))
+    return cos_sim
 
-    results = pose.process(imgRGB)
-    if results.pose_landmarks:
-        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+def read_poses(image1, image2):
+    imgRGB1 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
+    pose1 = pose.process(imgRGB1)
 
-    cv2.imwrite(DEST_DIR + filename, img)
+    imgRGB2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
+    pose2 = pose.process(imgRGB2)
+
+    return pose1, pose2
+
+def match(filename1, filename2):
+    image1 = cv2.imread(SOURCE_DIR + filename1)
+    image2 = cv2.imread(SOURCE_DIR + filename2)
+    pose1, pose2 = read_poses(image1, image2)
+
+    if pose1.pose_landmarks:
+        mpDraw.draw_landmarks(image1, pose1.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        print("Pose 1")
+        print(pose1.pose_landmarks)
+
+    cv2.imwrite(DEST_DIR + filename1, image1)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Estimate pose for a given image')
-    parser.add_argument('filename', action='store', default='dance.jpg', help='name of file in source folder to process')
+    parser.add_argument('filename1', action='store', default='dance.jpg', help='name of original file with pose data')
+    parser.add_argument('filename2', action='store', default='dance.jpg', help='name of the file we want to compare to original')
 
     args = parser.parse_args()
-    filename = args.filename
+    filename1 = args.filename1
+    filename2 = args.filename1
 
-    match(filename)
+    match(filename1, filename2)
