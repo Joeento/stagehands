9df86d3a0c8f7fb077cd050589cdfc4ba93e6d63
Eric Kudler <joeento@gmail.com>
2023-01-02 20:43:05 -0500

    Cleaned up pose matching code for simplicity

--- match.py
+++ match.py
@@ -7,25 +7,18 @@ mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 mpDraw = mp.solutions.drawing_utils
 
+SOURCE_DIR ='source/'
+DEST_DIR = 'dest/'
 
 def match(filename):
-    source_dir ='source/'
-    dest_dir = 'dest/'
-
-    img = cv2.imread(source_dir + filename)
+    img = cv2.imread(SOURCE_DIR + filename)
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = pose.process(imgRGB)
 
+    results = pose.process(imgRGB)
     if results.pose_landmarks:
         mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
 
-    for id, lm in enumerate(results.pose_landmarks.landmark):
-        h, w,c = img.shape
-        print(id, lm)
-        cx, cy = int(lm.x*w), int(lm.y*h)
-        cv2.circle(img, (cx, cy), 5, (255,0,0), cv2.FILLED)
-
-    cv2.imwrite(dest_dir + filename, img)
+    cv2.imwrite(DEST_DIR + filename, img)
 
 
 if __name__ == "__main__":
